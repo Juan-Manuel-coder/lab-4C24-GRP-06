949e3933e720f64e6ad79659d539cfb5bd32aacb
David Nikolas Rios Luque <david.rios.l@tecsup.edu.pe>
2026-08-29 23:14:37 -0500

    Actualizar clases de colaborador en sprint-1 desde casa

--- src/StudentReport.java
+++ src/StudentReport.java
@@ -1,1 +1,2 @@
 // Cambio nuevo en StudentReport para el sprint 1
+// Cambio final en casa para Sprint 1
--- src/StudentView.java
+++ src/StudentView.java
@@ -1,1 +1,2 @@
 // Cambio nuevo en StudentView para el sprint 1
+// Cambio final en casa para Sprint 1
